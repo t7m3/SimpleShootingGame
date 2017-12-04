44f27bbbdf6ffb32e2bd035ed955b6c2569d683c
NJC-T1 <toda7.mk3@gmail.com>
2017-12-04 13:23:40 +0900

    2017-12-04の２回目コミット 画像のファイル名を小文字に

--- app/src/main/java/com/example/njc_t1/simpleshootinggame/MainActivity.java
+++ app/src/main/java/com/example/njc_t1/simpleshootinggame/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.njc_t1.simpleshootinggame;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public  class  GameTimer extends CountDownTimer{
+
+        //GameTimerクラスのコンストラクタ
+        public GameTimer(long millisInFuture, long countDownInterval){
+            super(millisInFuture, countDownInterval );
+        }
+
+        //タイマイベントの処理
+        public void onTick(long millisUntilFinished){
+
+        }
+
+        //タイマ終了の処理
+        public  void onFinish(){
+
+        }
     }
 }
